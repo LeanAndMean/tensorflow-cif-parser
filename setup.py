35ffa3e8f6858885df640c36dd14dd42a58c1c59
Kevin Ryan <KevinRyan7926@gmail.com>
2019-06-05 16:22:35 -0400

    Added setup.py and setup.cfg files.

---
+++ setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='tensorflow-cif-parser',
+    version='0.1',
+    description='A Crystal Information File (CIF) parser in the TensorFlow'
+        ' Datasets API.',
+    url='http://github.com/LeanAndMean/tensorflow-cif-parser',
+    author='Kevin Ryan',
+    author_email='KevinRyan7926+tensorflow-cif-parser@gmail.com',
+    license='BSD-3',
+    packages=find_packages(),
+    install_requires=[
+        "tensorflow",
+        "ase",
+    ],
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest","pyinstrument"],
+    include_package_data=True,
+    zip_safe=False
+)
