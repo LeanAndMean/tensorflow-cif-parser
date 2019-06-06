e1c7a412b4874eec3e4beccac95dd412fb5a0759
Kevin Ryan <KevinRyan7926@gmail.com>
2019-06-06 16:07:51 -0400

    Improved docstring in test and fixed style in some files.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name='tensorflow-cif-parser',
     version='0.1',
     description='A Crystal Information File (CIF) parser in the TensorFlow'
-        ' Datasets API.',
+                ' Datasets API.',
     url='http://github.com/LeanAndMean/tensorflow-cif-parser',
     author='Kevin Ryan',
     author_email='KevinRyan7926+tensorflow-cif-parser@gmail.com',
@@ -17,7 +17,7 @@ setup(
         "ase",
     ],
     setup_requires=["pytest-runner"],
-    tests_require=["pytest","pyinstrument"],
+    tests_require=["pytest", "pyinstrument"],
     include_package_data=True,
     zip_safe=False
 )
--- tests/cif/parser/test_ase.py
+++ tests/cif/parser/test_ase.py
@@ -5,6 +5,12 @@ from tensorflow_cif_parser.cif.parser.ase import parse_file as ase_parser
 
 @profile_with_pytinstrument
 def test_ase_parser(cif_path):
+    """Loads crystal structure and prints some information.
+    Parameters
+    ----------
+    cif_path: str
+        Path to Crystal Information File (CIF).
+    """
     print("Path of test CIF:", str(cif_path))
     ase_atoms = ase_parser(cif_path)
     print("ASE atoms object:")
